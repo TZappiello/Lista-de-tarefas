5a0be0662fdaa632025b6e42356d86e19190ba59
Tzappiello <tzappiello@gmail.com>
2022-08-15 15:52:23 -0300

    Criação do projeto e configuração de Rotas

---
+++ src/app/modules/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-itens',
+  templateUrl: './todo-input-add-itens.component.html',
+  styleUrls: ['./todo-input-add-itens.component.scss']
+})
+export class TodoInputAddItensComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { HeaderComponent } from '../components/header/header.component';
+import { TodoButtonDeleteAllComponent } from '../components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItensComponent } from '../components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from '../components/todo-list/todo-list.component';
+
+//Page
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItensComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
